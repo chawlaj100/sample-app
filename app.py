bb7777bdf7c8f29fb3fae2884c03c487db2a482e
Jatin Chawla <chawlaj00@gmail.com>
2021-06-05 12:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
     return "SAMPLE WEB APP"
 
-app.route("/hello")
+@app.route("/hello")
 def hello():
     return "HELLO MS-RANGERS"
