e656c402f77dabc6960f51e60e23848338bd2291
chawlaj100 <chawlaj00@gmail.com>
2021-06-05 12:08:08 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route('/')
+def index():
+    return ("SAMPLE WEB APP")
+
+app.route('/hello')
+def hello():
+    return ("HELLO MS-RANGERS")
